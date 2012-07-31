1cbbad3e145c4ebd91b8c2ace89bd302b1408e0a
Daniel Friesel <derf@finalrewind.org>
2012-07-31 16:43:49 +0200

    support item editing

--- cgi/index.pl
+++ cgi/index.pl
@@ -18,13 +18,13 @@ sub handle_request {
 
 	my $action = $self->param('action') // q{};
 	my $item   = $self->param('item') // q{};
-	my $file   = $self->param('file') // q{};
+	my $afile  = $self->param('file') // q{};
 	my $args   = $self->param('args') // q{};
 
 	my $items;
 
 	if ($action eq 'add') {
-		$self->render( 'add', file => $file );
+		$self->render( 'add', file => $afile );
 		return;
 	}
 
@@ -35,13 +35,21 @@ sub handle_request {
 		my $xp_note = XML::LibXML::XPathExpression->new('/todo/note');
 
 		for my $note ( $xml->findnodes($xp_note) ) {
+			my $time = $note->getAttribute('time');
+
 			if ($action eq 'done' and $note->getAttribute('time') eq $item) {
 				$note->setAttribute('done', time());
 				$changed = 1;
 			}
+			if ($action eq 'edit' and $file eq $afile and $item eq $time) {
+				my $text = $note->textContent;
+				$text =~ s{^\s+}{};
+				$text =~ s{\s+$}{};
+				$self->param(tdata => $text);
+				$self->render( 'edit', file => $file, item => $item);
+				return;
+			}
 			if (not $note->getAttribute('done')) {
-				my $time = $note->getAttribute('time');
-
 				push(@{$items->{$file}}, {
 					text => $note->textContent,
 					time => $time,
@@ -72,6 +80,26 @@ get '/_add' => sub {
 	$new->appendText($str);
 
 	$root->appendChild($new);
+
+	$xml->toFile("${prefix}/${file}", 0);
+
+	$self->redirect_to('/');
+};
+
+get '/_edit' => sub {
+	my $self = shift;
+	my $file = $self->param('file');
+	my $item = $self->param('item');
+	my $str  = $self->param('tdata');
+
+	my $xml = XML::LibXML->load_xml( location => "${prefix}/${file}" );
+	my ($node) = $xml->findnodes("/todo/note[\@time=\"$item\"]");
+	my $new = XML::LibXML::Element->new('note');
+	$new->setAttribute('priority', 'medium');
+	$new->setAttribute('time', $item);
+	$new->appendText($str);
+
+	$node->replaceNode($new);
 
 	$xml->toFile("${prefix}/${file}", 0);
 
@@ -113,6 +141,33 @@ __DATA__
 <%= text_field 'file' %><br/>
 <%= text_field 'data' %><br/>
 <%= submit_button 'add' %>
+</div>
+<% end %>
+</div>
+</body>
+</html>
+
+@@ edit.html.ep
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gtd-web</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+
+	body {
+		font-family: Sans-Serif;
+	}
+	</style>
+</head>
+<body>
+<div>
+<%= form_for _edit => begin %>
+<div>
+<%= text_field 'file' %><br/>
+<%= text_field 'item' %><br/>
+<%= text_field 'tdata' %><br/>
+<%= submit_button 'done' %>
 </div>
 <% end %>
 </div>
