1b5e1a64b3421696408e6afb33b9041d51c63253
Daniel Friesel <derf@finalrewind.org>
2013-03-02 12:28:24 +0100

    lowe accept interval, code cleanup

--- cgi/index.pl
+++ cgi/index.pl
@@ -17,13 +17,13 @@ sub handle_request {
 	my $self = shift;
 
 	my $action = $self->param('action') // q{};
-	my $item   = $self->param('item') // q{};
-	my $afile  = $self->param('file') // q{};
-	my $args   = $self->param('args') // q{};
+	my $item   = $self->param('item')   // q{};
+	my $afile  = $self->param('file')   // q{};
+	my $args   = $self->param('args')   // q{};
 
 	my $items;
 
-	if ($action eq 'add') {
+	if ( $action eq 'add' ) {
 		$self->render( 'add', file => $afile );
 		return;
 	}
@@ -31,35 +31,42 @@ sub handle_request {
 	for my $file (qw(later someday todo uni waiting)) {
 
 		my $changed = 0;
-		my $xml = XML::LibXML->load_xml( location => "${prefix}/${file}" );
+		my $xml     = XML::LibXML->load_xml( location => "${prefix}/${file}" );
 		my $xp_note = XML::LibXML::XPathExpression->new('/todo/note');
 
 		for my $note ( $xml->findnodes($xp_note) ) {
 			my $time = $note->getAttribute('time');
 
-			if ($action eq 'done' and $note->getAttribute('time') eq $item) {
-				$note->setAttribute('done', time());
+			if ( $action eq 'done' and $note->getAttribute('time') eq $item ) {
+				$note->setAttribute( 'done', time() );
 				$changed = 1;
 			}
-			if ($action eq 'edit' and $file eq $afile and $item eq $time) {
+			if ( $action eq 'edit' and $file eq $afile and $item eq $time ) {
 				my $text = $note->textContent;
 				$text =~ s{^\s+}{};
 				$text =~ s{\s+$}{};
-				$self->param(tdata => $text);
-				$self->render( 'edit', file => $file, item => $item);
+				$self->param( tdata => $text );
+				$self->render(
+					'edit',
+					file => $file,
+					item => $item
+				);
 				return;
 			}
-			if (not $note->getAttribute('done')) {
-				push(@{$items->{$file}}, {
-					text => $note->textContent,
-					time => $time,
-					link => "?item=$time&file=$file&",
-				});
+			if ( not $note->getAttribute('done') ) {
+				push(
+					@{ $items->{$file} },
+					{
+						text => $note->textContent,
+						time => $time,
+						link => "?item=$time&file=$file&",
+					}
+				);
 			}
 		}
 
 		if ($changed) {
-			$xml->toFile("${prefix}/${file}", 0);
+			$xml->toFile( "${prefix}/${file}", 0 );
 		}
 	}
 
@@ -75,13 +82,13 @@ get '/_add' => sub {
 	my ($root) = $xml->findnodes('/todo');
 
 	my $new = XML::LibXML::Element->new('note');
-	$new->setAttribute('priority', 'medium');
-	$new->setAttribute('time', time());
+	$new->setAttribute( 'priority', 'medium' );
+	$new->setAttribute( 'time',     time() );
 	$new->appendText($str);
 
 	$root->appendChild($new);
 
-	$xml->toFile("${prefix}/${file}", 0);
+	$xml->toFile( "${prefix}/${file}", 0 );
 
 	$self->redirect_to('/');
 };
@@ -95,22 +102,23 @@ get '/_edit' => sub {
 	my $xml = XML::LibXML->load_xml( location => "${prefix}/${file}" );
 	my ($node) = $xml->findnodes("/todo/note[\@time=\"$item\"]");
 	my $new = XML::LibXML::Element->new('note');
-	$new->setAttribute('priority', 'medium');
-	$new->setAttribute('time', $item);
+	$new->setAttribute( 'priority', 'medium' );
+	$new->setAttribute( 'time',     $item );
 	$new->appendText($str);
 
 	$node->replaceNode($new);
 
-	$xml->toFile("${prefix}/${file}", 0);
+	$xml->toFile( "${prefix}/${file}", 0 );
 
 	$self->redirect_to('/');
 };
 
 app->config(
 	hypnotoad => {
-		listen => ['http://*:8094'],
-		pid_file => '/tmp/gtd-web.pid',
-		workers => 1,
+		accept_interval => 0.2,
+		listen          => ['http://*:8094'],
+		pid_file        => '/tmp/gtd-web.pid',
+		workers         => 1,
 	},
 );
 
